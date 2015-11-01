cb874a52ed695c36ae38e9ffb15f030d21d28186
= <=>
2015-11-01 14:52:00 -0500

    I think I fixed the merge problem by changing the static path in settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -83,4 +83,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
