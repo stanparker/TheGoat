fe637bc7829b2d2ff23e03bf7c33a1b54cb87eaa
= <=>
2015-11-12 14:44:16 -0500

    Fixed typo in fabfile, hoping it runs properly now

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -29,18 +29,18 @@ def _get_latest_source(source_folder):
 
 def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
-    sed(settings_path, "DEBUG = True", "DEBUG = False")
+    sed(settings_path, "DEBUG = True", "DEBUG = False")  #1
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
-        'ALLOWED_HOSTS = ["%s"]' % (site_name,)
+        'ALLOWED_HOSTS = ["%s"]' % (site_name,)  #2
     )
     secret_key_file = source_folder + '/superlists/secret_key.py'
-    if not exists (secret_key_file):
-        chars = 'abcdefghizklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    if not exists(secret_key_file):  #3
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, "SECRET_KEY '%s'" % (key,))
-    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
-
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')  #4
+    
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
